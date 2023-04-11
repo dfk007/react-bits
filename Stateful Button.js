8d97d532c1d4e8604130703a4d79a6fd38126ae5
dfk007 <df89@student.london.ac.uk>
2023-04-12 04:23:46 +0500

    Create Stateful Button.js

---
+++ Stateful Button.js
@@ -0,0 +1,11 @@
+function Button() {
+  // a,b = useState()
+  // counter, setCounter = useState()
+  const [counter, setCounter] = useState(0);
+  return <button onClick={() => setCounter(counter+1)}>{counter}</button>;
+}
+
+ReactDOM.render(
+  <Button />, 
+  document.getElementById('mountNode')
+)
